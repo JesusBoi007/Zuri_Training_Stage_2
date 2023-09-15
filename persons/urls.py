4b869ce27635cdf2d1eaab4511a46d7d445ba36c
JesusBoi007 <jesusboi008@gmail.com>
2023-09-15 20:27:52 +0100

    third commit

--- persons/urls.py
+++ persons/urls.py
@@ -3,6 +3,6 @@ from .views import PersonListCreateView, PersonRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('api/', PersonListCreateView.as_view(), name='person-list-create'),
-    path('api/<int:pk>', PersonRetrieveUpdateDestroyView.as_view(), name='person-detail'),
+    path('api/<int:pk>/', PersonRetrieveUpdateDestroyView.as_view(), name='person-detail'),
 ]
 
